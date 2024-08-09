06881039a2773ab18f9fb5b54cd1b37743789d61
jboyko <jamesboyko21@gmail.com>
2024-08-08 21:23:28 -0400

    docs and checks

--- R/plotting.R
+++ R/plotting.R
@@ -1,6 +1,18 @@
-plot.count.specimens <- function(metadata) {
+#' Plot the Number of Specimens by Institution
+#'
+#' This function takes metadata containing specimen information and plots the
+#' number of specimens for each institution as a bar plot.
+#'
+#' @param metadata A data frame containing at least the column \code{institutionCode},
+#' which lists the institution associated with each specimen.
+#'
+#' @return A bar plot showing the number of specimens for each institution.
+#' @importFrom graphics barplot
+#' @export
+#'
+plot_specimens_by_institution <- function(metadata) {
   sorted_counts <- sort(table(metadata$institutionCode), decreasing = TRUE)
-  barplot(sorted_counts, main = "Specimens by Institution", 
-          xlab = "", ylab = "Number of Specimens",
-          col = "lightblue", las = 2)
+  barplot(sorted_counts, main = "Specimens by Institution",
+    xlab = "", ylab = "Number of Specimens",
+    col = "lightblue", las = 2)
 }
