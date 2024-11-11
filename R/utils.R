91e476d9fa863efb146a8dcf7bc21e19199c27da
James Boyko <jamesboyko21@gmail.com>
2024-11-12 00:58:45 +0100

    working on windows

--- R/utils.R
+++ R/utils.R
@@ -87,7 +87,12 @@ download_file_safe <- function(url, destfile) {
     }
   }
   # Download the file with the new or original name
-  download.file(url, destfile)
+  if(Sys.info()['sysname'] == "Windows"){
+    download.file(url, destfile, "libcurl", mode = "wb")
+  }else{
+    download.file(url, destfile)
+  }
+  
   #destfile <- gsub(paste(c(".*\\/", paste0(".",file_extension)), collapse="|"), "", destfile)
   # Return the name of the file that was saved
   return(destfile)
